fbd450a695724d7a1533a6896d476fd8971626b9
Malak Naim <malak.r.naim@gmail.com>
2022-08-21 08:54:28 +0300

    رر

--- Magazine-Palpay/Controllers/HomeController.cs
+++ Magazine-Palpay/Controllers/HomeController.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
- 
 namespace Magazine_Palpay.Controllers
 {
     public class HomeController : BaseController
