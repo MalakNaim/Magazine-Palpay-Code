b8b6b08c2b29aa2c4c8b1e8e37552a3494e4adbc
Malak Naim <malak.r.naim@gmail.com>
2023-01-05 11:06:51 +0200

    changes

--- Magazine-Palpay/Program.cs
+++ Magazine-Palpay/Program.cs
@@ -1,32 +1,103 @@
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
+using Magazine_Palpay.Web;
+using Magazine_Palpay.Web.Extensions;
+using Magazine_Palpay.Web.IdentityModels;
+using Magazine_Palpay.Web.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Magazine_Palpay
+var builder = WebApplication.CreateBuilder(args);
+var _config = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
+var extensionsPath = environment.ContentRootPath + _config["Extensions:Path"];
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseOracle(_config.GetConnectionString("oracle"),
+           e => e.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)
+           .UseOracleSQLCompatibility("11"))).AddIdentity<FluentUser, FluentRole>(options =>
+           {
+               options.Password.RequiredLength = 6;
+               options.Password.RequireDigit = false;
+               options.Password.RequireLowercase = false;
+               options.Password.RequireNonAlphanumeric = false;
+               options.Password.RequireUppercase = false;
+               options.User.RequireUniqueEmail = true;
+               options.SignIn.RequireConfirmedAccount = true;
+
+           })
+               .AddEntityFrameworkStores<ApplicationDbContext>()
+               .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            try {
-                CreateHostBuilder(args).Build().Run();
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 
-            }
-            catch (Exception ex) { 
+}); 
+builder.Services.AddDatabaseDeveloperPageExceptionFilter(); 
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient<IExchangeRateService, ExchangeRateService>(c =>
+{
+    c.BaseAddress = new Uri("https://api.exchangerate.host/");
+});
+builder.Services.AddNotyf(config =>
+{
+    config.DurationInSeconds = 30;
+    config.IsDismissable = true;
+    config.Position = NotyfPosition.BottomLeft;
+});
+builder.Services.AddSingleton<GlobalExceptionHandler>();
+builder.Services.AddNotyf(config =>
+{
+    config.DurationInSeconds = 30;
+    config.IsDismissable = true;
+    config.Position = NotyfPosition.BottomLeft;
+});
 
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+app.UseNotyf();
+if (environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+});
+app.UseMiddleware<GlobalExceptionHandler>();
+
+app.UseEndpoints(endpoints =>
+{
+    //  endpoints.MapRazorPages();
+    endpoints.MapControllerRoute(
+      // name: "areas",
+      name: "default",
+      pattern: "{controller=Home}/{action=Index}");
+    //pattern: "{area:Admin}/{controller=Home}/{action=Index}/{id?}"
+    // );
+}); 
+app.Run();
